ad68fbf33b776f2ed2b134f623f3012002210906
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2024-02-13 22:05:57 +0100

    pdk params -> hiera linter

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,17 +1,27 @@
 # Private class, do not use directly.
 # Takes care about managing the service.
-
+#
+# [*service_enable*]
+#   Boolean: whether to enable the service or not, default: true
+#
+# [*service_ensure*]
+#   Enum[running, stopped]: the desired state of bootparamd, default: running
+#
+# [*service_flags*]
+#   String: the service flags, default: '-s'
+#
+# [*service_name*]
+#   String: the name of the service, default: bootparamd
+#
 class bootparamd::service (
-  $service_enable,
-  $service_ensure,
-  $service_flags,
-  $service_name,
-){
-
+  Boolean $service_enable,
+  Enum[running, stopped, 'running', 'stopped'] $service_ensure,
+  String $service_flags,
+  String $service_name,
+) {
   if ! defined(Service['portmap']) {
     include portmap
   }
-
   service { $service_name:
     ensure     => $service_ensure,
     enable     => $service_enable,
@@ -22,5 +32,4 @@ class bootparamd::service (
 
   Service['portmap']
   -> Service[$service_name]
-
 }
--- manifests/sysctl.pp
+++ manifests/sysctl.pp
@@ -1,9 +1,13 @@
 # Private class, do not use directly.
 # Takes care about the syctl, net.inet.ip.portlast,
 # required to boot old SGI boxes.
-
+#
+# [*old_sgi_client*]
+#   Boolean: enables sysctl to lock down high ports for bootparamd
+#   to listen on. default: false
+#
 class bootparamd::sysctl (
-  $old_sgi_client,
+  Boolean $old_sgi_client,
 ) {
   if $old_sgi_client {
     sysctl { 'net.inet.ip.portlast':
